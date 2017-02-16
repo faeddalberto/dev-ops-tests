23d90a662f9587f06a1dd8b9db95796420c165ae
Alberto Faedda <faeddalberto@gmail.com>
2017-02-16 12:42:37 +0000

    created spring boot ms for user management

---
+++ user-mgmt-ms/src/main/java/com/faeddalberto/UserMgmtMsApplication.java
@@ -0,0 +1,12 @@
+package com.faeddalberto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMgmtMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMgmtMsApplication.class, args);
+	}
+}
