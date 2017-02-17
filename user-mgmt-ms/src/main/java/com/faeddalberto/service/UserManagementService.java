6562df6daed6afa3939d2ab82870175bed00866c
Alberto Faedda <faeddalberto@gmail.com>
2017-02-17 11:50:01 +0000

    add small change to test git triggering build on jenkinswq

--- user-mgmt-ms/src/main/java/com/faeddalberto/service/UserManagementService.java
+++ user-mgmt-ms/src/main/java/com/faeddalberto/service/UserManagementService.java
@@ -10,18 +10,15 @@ public class UserManagementService implements UserManagement {
     @RequestMapping("/addUser")
     @Override
     public void addUser(@RequestParam(value="jsonUser") String jsonUser) {
-
     }
 
     @RequestMapping("/deleteUser")
     @Override
     public void deleteUser(@RequestParam(value="username") String username) {
-
     }
 
     @RequestMapping("/searchUser")
     @Override
     public void searchUser(@RequestParam(value="username") String username) {
-
     }
 }
